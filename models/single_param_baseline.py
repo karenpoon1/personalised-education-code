8d2297457ecba8a4a8a9676c2e229d8121f24637
karenpoon1 <ylkp2@cam.ac.uk>
2021-11-17 02:00:47 +0000

    update file path

--- models/single_param_baseline.py
+++ models/single_param_baseline.py
@@ -1,7 +1,7 @@
 import torch
 
-# df = pd.read_csv("personalised-education/Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv", skiprows=list(range(1,24)), usecols=list(range(2,26)))
-# max_scores = pd.read_csv("personalised-education/Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv", nrows = 1, skiprows=[1], usecols=list(range(2,26)))
+# df = pd.read_csv("Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv", skiprows=list(range(1,24)), usecols=list(range(2,26)))
+# max_scores = pd.read_csv("Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv", nrows = 1, skiprows=[1], usecols=list(range(2,26)))
 
 def train_single_param(first_quadrant_ts, train_question_ts, train_student_ts, test_ts, seed_number):
 
