e5be8f93f9c41e40c333391bb802b44df9103804
karenpoon1 <ylkp2@cam.ac.uk>
2021-11-28 11:43:26 +0000

    predict from ground truth

--- synthetic_data_ADP.py
+++ synthetic_data_ADP.py
@@ -2,10 +2,11 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 # from torch.autograd import Variable
 from utils.split_data import split_to_4quadrants
-from ability_difficulty_product import probit_correct, train_product_alternate_quadrants
+from models.ability_difficulty_product import probit_correct, train_product_alternate_quadrants
 
 
 def synthesise_data_from_normal(S, Q, rng):
@@ -21,14 +22,50 @@ def synthesise_data_from_normal(S, Q, rng):
     portion = portion[22511:22511+50, 12:]
     sns.heatmap(portion, linewidth=0.5)
     plt.title('Synthetic probabilities')
+    plt.xlabel('Questions')
+    plt.ylabel('Students')
     plt.show()
 
     fake_data = torch.bernoulli(product_params_matrix, generator=rng)
+    
+    rng.manual_seed(1000)
+    predictions_from_ground_truth = torch.bernoulli(product_params_matrix, generator=rng)
+
+    _,_,_,fake_data_test_set = split_to_4quadrants(fake_data, student_split=0.5, question_split=0.5)
+    _,_,_,predictions_from_ground_truth_test_set = split_to_4quadrants(predictions_from_ground_truth, student_split=0.5, question_split=0.5)
+
+    performance = torch.sum(torch.eq(fake_data_test_set, predictions_from_ground_truth_test_set)) / torch.numel(fake_data_test_set)
+    performance = float(performance)*100
+    print(performance)
+
+    fake_data_test_set_reshaped = fake_data_test_set.reshape(-1).type(torch.int)
+    predictions_from_ground_truth_test_set_reshaped = predictions_from_ground_truth_test_set.reshape(-1).type(torch.int)
+
+    conf_matrix = confusion_matrix(fake_data_test_set_reshaped.numpy(), predictions_from_ground_truth_test_set_reshaped.detach().numpy())
+    conf_matrix = conf_matrix*100/torch.numel(fake_data_test_set)
+    print(conf_matrix)
+    
+    real_portion = fake_data_test_set.detach()
+    real_portion = real_portion[:50, :]
+    sns.heatmap(real_portion, linewidth=0.5)
+    plt.title('Synthesised data')
+    plt.xlabel('Questions')
+    plt.ylabel('Students')
+    plt.show()
+
+    predicted_portion = predictions_from_ground_truth_test_set.detach()
+    predicted_portion = predicted_portion[:50, :]
+    sns.heatmap(predicted_portion, linewidth=0.5)
+    plt.title('Predicted output')
+    plt.xlabel('Questions')
+    plt.ylabel('Students')
+    plt.show()
+
     return fake_data, bs_tensor_fake, bq_tensor_fake, portion
 
 
 learning_rate = 0.0003
-n_iters = 80
+n_iters = 50000
 seed_number = 1000
 
 rng = torch.Generator()
@@ -40,13 +77,15 @@ Q = 24
 fake_data, bs_tensor_fake, bq_tensor_fake, portion_fake = synthesise_data_from_normal(S, Q, rng)
 first_quadrant, train_question_output_ts, train_student_output_ts, test_output_ts = split_to_4quadrants(fake_data, student_split=0.5, question_split=0.5)
 
-bs_tensor, bq_tensor, product_params_matrix, performance = train_product_alternate_quadrants(train_question_output_ts, train_student_output_ts, test_output_ts, learning_rate, n_iters, rng)
+bs_tensor, bq_tensor, product_params_matrix, performance, conf_matrix = train_product_alternate_quadrants(train_question_output_ts, train_student_output_ts, test_output_ts, learning_rate, n_iters, rng)
 
 portion = product_params_matrix.detach()
 portion = portion[:50, :]
 portion_diff = portion - portion_fake
 sns.heatmap(portion_diff, linewidth=0.5)
 plt.title('Error')
+plt.xlabel('Questions')
+plt.ylabel('Students')
 plt.show()
 
 # print(f"bs_fake (student params): {bs_tensor_fake[22511:22511+50]}")
