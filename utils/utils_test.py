068d72557b6517ee0168a90e58a59666b01819b3
karenpoon1 <ylkp2@cam.ac.uk>
2021-12-16 19:55:52 +0000

    update naming and comments

--- utils/utils_test.py
+++ utils/utils_test.py
@@ -1,4 +1,5 @@
 import unittest
+from scipy.sparse import data
 import torch
 
 from split_and_vectorise import split_and_vectorise
@@ -6,24 +7,25 @@ from split_and_vectorise import split_and_vectorise
 class TestUtils(unittest.TestCase):
 
     def test_split_and_vectorise(self):
-        data_ts = torch.tensor([[1, 1, 0, 1, 0],
-                                [1, 1, 0, 1, 0],
-                                [1, 1, 0, 1, 0],
-                                [0, 1, 1, 1, 0],
-                                [0, 1, 0, 0, 0]],
+        data_ts = torch.tensor([[1, 2, 3, 4],
+                                [5, 6, 7, 8],
+                                [9, 10, 11, 12],
+                                [13, 14, 15, 16]],
                                 dtype=torch.float)
-        test_range = [[2,5], [2,5]]
+        testset_row_range, testset_col_range = [2,4], [2, 4] # Split matrix [row range, column range] out as test set
+        
+        # First row: data
+        # Second row: row position
+        # Third row: column position
+        exp_train_vectorised_ts = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14],
+            [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3],
+            [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 0, 1]], dtype=torch.int32)
+        exp_test_vectorised_ts = torch.tensor([[11, 12, 15, 16],
+            [2, 2, 3, 3],
+            [2, 3, 2, 3]], dtype=torch.int32)
 
-        exp_train_vectorised_ts = torch.tensor([[1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1],
-            [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4],
-            [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 0, 1, 0, 1]], dtype=torch.int32)
-        exp_test_vectorised_ts = torch.tensor([[0, 1, 0, 1, 1, 0, 0, 0, 0],
-            [2, 2, 2, 3, 3, 3, 4, 4, 4],
-            [2, 3, 4, 2, 3, 4, 2, 3, 4]], dtype=torch.int32)
-
-        train_vectorised_ts, test_vectorised_ts = split_and_vectorise(data_ts, test_range, shuffle=False)
+        train_vectorised_ts, test_vectorised_ts = split_and_vectorise(data_ts, testset_row_range, testset_col_range, shuffle=False)
         self.assertTrue(torch.equal(train_vectorised_ts, exp_train_vectorised_ts) and torch.equal(test_vectorised_ts, exp_test_vectorised_ts))
 
 if __name__ == '__main__':
-    torch.manual_seed(0)
     unittest.main()
