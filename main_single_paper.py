3b3d67a97dee1d048681aa102070772c76b50642
karenpoon1 <ylkp2@cam.ac.uk>
2021-12-16 21:05:26 +0000

    create class for ADP model

--- main_single_paper.py
+++ main_single_paper.py
@@ -2,26 +2,37 @@ import pandas as pd
 
 from utils.parse_data import parse_paper_data
 from run_model import Models
-
-# old paper
-old_paper_csv = "Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv"
-old_paper_start = 23
-
-# new paper
-new_paper_csv = "Fwd__Pinpoint_ML_Dataset\9to1_2017_GCSE_1H_and_2H_and_3H Linked Pinpoint Data_Cleaned.csv"
-new_paper_start = 6
+from models.ADP import ADP
 
 paper1_columns = ['Name', 'q1', 'q2', 'q3',
                       'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11', 'q12', 'q13', 'q14',
                       'q15', 'q16', 'q17', 'q18', 'q19', 'q20', 'q21', 'q22', 'q23', 'q24']
 
-raw_data = pd.read_csv(new_paper_csv, low_memory=False)
-exam_data_df, meta_data_df = parse_paper_data(raw_data, new_paper_start, paper1_columns)
+# old paper
+old_paper_csv = "Fwd__Pinpoint_ML_Dataset/9to1_2017_GCSE_1H.csv"
+old_paper_start = 23
 
-my_models = Models(exam_data_df, meta_data_df)
-# my_models.ADP_vectorised(iters=4550)
-# my_models.meta(iters=1520)
-my_models.interactive(shuffle=True, iters=100)
+old_raw_data = pd.read_csv(old_paper_csv, low_memory=False)
+old_exam_data_df, old_meta_data_df = parse_paper_data(old_raw_data, old_paper_start, paper1_columns)
 
-# my_models.ADP_vectorised(learning_rate=0.0002, iters=5930)
-# my_models.meta(iters=1530)
+# old_data_models = Models(old_exam_data_df, old_meta_data_df)
+# old_data_models.ADP_vectorised(learning_rate=0.0002, iters=5930)
+# old_data_models.meta(iters=1530)
+# old_data_models.interactive(learning_rate=0.0002 ,iters=6000)
+
+# new paper
+new_paper_csv = "Fwd__Pinpoint_ML_Dataset\9to1_2017_GCSE_1H_and_2H_and_3H Linked Pinpoint Data_Cleaned.csv"
+new_paper_start = 6
+
+new_raw_data = pd.read_csv(new_paper_csv, low_memory=False)
+new_exam_data_df, new_meta_data_df = parse_paper_data(new_raw_data, new_paper_start, paper1_columns)
+
+# new_data_models = Models(new_exam_data_df, new_meta_data_df)
+# new_data_models.ADP_vectorised(iters=20000)
+# new_data_models.ADP_vectorised(iters=4550)
+# new_data_models.meta(iters=1520)
+# new_data_models.interactive(iters=2)
+
+S, Q = new_exam_data_df.shape[0], new_exam_data_df.shape[1] # Data block size
+my_ADP = ADP(new_exam_data_df, new_meta_data_df, [int(S/2), S], [int(Q/2), Q])
+my_ADP.run(learning_rate=0.00025, iters=4550)
---
+++ models/ADP.py
@@ -0,0 +1,162 @@
+import torch
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+from utils.clean_data import thres_score_range
+from utils.binarise_data import binarise_by_mid, binarise_by_avg
+from utils.shuffle_data import shuffle_cols
+from utils.split_and_vectorise import split_and_vectorise
+
+class ADP:
+    def __init__(self, raw_data_df, raw_meta_df, testset_row_range, testset_col_range, binarise_method='mid', shuffle=True, seed_number=1000) -> None:
+        self.raw_data_df = raw_data_df
+        self.raw_meta_df = raw_meta_df
+        self.max_scores = raw_meta_df.loc['Max'].astype(float)
+        
+        self.testset_row_range = testset_row_range
+        self.testset_col_range = testset_col_range
+
+        self.binarise_method = binarise_method
+        self.shuffle = shuffle
+
+        rng = torch.Generator()
+        rng.manual_seed(seed_number)
+        self.rng = rng
+
+    @staticmethod
+    def probit_correct(bs, bq):
+        return 1/(1+torch.exp(-bs-bq))
+
+    def run(self, learning_rate, iters):
+        _, processed_ts = self.process_raw()
+        train_vectorised_ts, test_vectorised_ts = split_and_vectorise(processed_ts, self.testset_row_range, self.testset_col_range)
+        
+        S, Q = processed_ts.shape[0], processed_ts.shape[1] # Data block size
+        trained_bs, trained_bq, nll_train_arr, nll_test_arr, acc_arr = self.train(train_vectorised_ts, test_vectorised_ts, S, Q, learning_rate, iters)
+        accuracy, conf_matrix = self.predict(trained_bs, trained_bq, test_vectorised_ts, vis=True)
+
+        self.plot_result(nll_train_arr/train_vectorised_ts.shape[1], nll_test_arr/test_vectorised_ts.shape[1], acc_arr, iters)
+        print(f"ADP vectorised (rate={learning_rate}, iters={iters}, binarise={self.binarise_method}, shuffle={self.shuffle}) -> accuracy: {accuracy}, confusion matrix: \n{conf_matrix}")
+        
+
+    def process_raw(self):
+        thres_df = thres_score_range(self.raw_data_df, self.max_scores)
+        if self.binarise_method == 'mid':
+            binarised_df = binarise_by_mid(thres_df, self.max_scores)
+        elif self.binarise_method == 'avg':
+            binarised_df = binarise_by_avg(thres_df)
+        
+        if self.shuffle:
+            binarised_df = shuffle_cols(binarised_df)
+
+        return binarised_df, torch.tensor(binarised_df.values)
+
+
+    def train(self, train_ts, test_ts, S, Q, learning_rate, iters):
+        step_size = 25
+
+        nll_train_arr, nll_test_arr = np.zeros(iters), np.zeros(iters)
+        acc_arr = np.zeros(math.ceil(iters/step_size))
+
+        # Randomly initialise random student, question parameters
+        bs = torch.randn(S, requires_grad=True, generator=self.rng)
+        bq = torch.randn(Q, requires_grad=True, generator=self.rng)
+
+        for epoch in range(iters):
+            # Train set params
+            bs_train = torch.index_select(bs, 0, train_ts[1])
+            bq_train = torch.index_select(bq, 0, train_ts[2])
+            # Train nll
+            probit_1 = 1/(1+torch.exp(-bs_train-bq_train))
+            nll = -torch.sum(train_ts[0]*torch.log(probit_1) + (1-train_ts[0])*torch.log(1-probit_1))
+            nll.backward()
+
+            # Test set params
+            bs_copy, bq_copy = torch.clone(bs), torch.clone(bq)
+            bs_test = torch.index_select(bs_copy, 0, test_ts[1])
+            bq_test = torch.index_select(bq_copy, 0, test_ts[2])
+            # Test nll
+            probit_1_test = 1/(1+torch.exp(-bs_test-bq_test))
+            nll_test = -torch.sum(test_ts[0]*torch.log(probit_1_test) + (1-test_ts[0])*torch.log(1-probit_1_test))
+
+            # Gradient descent
+            with torch.no_grad():
+                bs -= learning_rate * bs.grad
+                bq -= learning_rate * bq.grad
+
+            # Zero gradients after updating
+            bs.grad.zero_()
+            bq.grad.zero_()
+
+            if epoch % step_size == 0:
+                acc, _ = self.predict(bs, bq, test_ts)
+                acc_arr[epoch // step_size] = acc
+                print(epoch, nll, nll_test, acc)
+
+            nll_train_arr[epoch], nll_test_arr[epoch] = nll, nll_test
+
+        print(epoch, nll, nll_test, acc)
+        return bs, bq, nll_train_arr, nll_test_arr, acc_arr
+
+
+    def predict(self, bs, bq, test_ts, vis=False):
+        # Test set params after training
+        bs_test = torch.index_select(bs, 0, test_ts[1])
+        bq_test = torch.index_select(bq, 0, test_ts[2])
+
+        predicted_probit = ADP.probit_correct(bs_test, bq_test)
+        predictions = torch.bernoulli(predicted_probit, generator=self.rng)
+
+        performance = torch.sum(torch.eq(test_ts[0], predictions)) / torch.numel(test_ts[0])
+        performance = float(performance)*100
+
+        conf_matrix = confusion_matrix(test_ts[0].numpy(), predictions.detach().numpy())
+        conf_matrix = conf_matrix*100/torch.numel(test_ts[0])
+        
+        if vis:
+            # Visualisation (only possible under certain conditions)
+            no_questions = self.testset_col_range[1] - self.testset_col_range[0]
+            test_ts_reshaped = test_ts[0].reshape(int(len(test_ts[0])/no_questions),no_questions)
+            predicted_probit_reshaped = predicted_probit.reshape(int(len(predicted_probit)/no_questions),no_questions)
+            predictions_reshaped = predictions.reshape(int(len(predictions)/no_questions),no_questions)
+            
+            real_portion = test_ts_reshaped.detach()
+            real_portion = real_portion[:50, :]
+            sns.heatmap(real_portion, linewidth=0.5)
+            plt.title('Real binarised data')
+            plt.xlabel('Questions')
+            plt.ylabel('Students')
+            plt.show()
+
+            predicted_probit_portion = predicted_probit_reshaped.detach()
+            predicted_probit_portion = predicted_probit_portion[:50, :]
+            sns.heatmap(predicted_probit_portion, linewidth=0.5)
+            plt.title('Predicted probabilities')
+            plt.xlabel('Questions')
+            plt.ylabel('Students')
+            plt.show()
+
+            predicted_portion = predictions_reshaped.detach()
+            predicted_portion = predicted_portion[:50, :]
+            sns.heatmap(predicted_portion, linewidth=0.5)
+            plt.title('Predicted output')
+            plt.xlabel('Questions')
+            plt.ylabel('Students')
+            plt.show()
+
+        return performance, conf_matrix
+
+
+    def plot_result(self, avg_nll_train_arr, avg_nll_test_arr, acc_arr, iters):
+        step_size = 25
+        plt.plot(range(iters), avg_nll_train_arr)
+        plt.plot(range(iters), avg_nll_test_arr)
+        plt.plot(np.arange(0, iters, step_size), acc_arr/100)
+        plt.title('Train and test nll')
+        plt.ylabel('Negative log likelihood')
+        plt.xlabel('epoch')
+        plt.legend(['Train nll', 'Test nll', 'Accuracy'])
+        plt.show()
